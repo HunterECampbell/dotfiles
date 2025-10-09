8acf000e554472a06541af4b6ce4054d282a6186
Hunter Campbell <hcnureth@gmail.com>
2025-10-09 16:49:19 -0600

    Update work setup

--- scripts/start_work_env.sh
+++ scripts/start_work_env.sh
@@ -2,7 +2,7 @@
 flatpak run com.google.Chrome --profile-directory="Profile 2" &
 flatpak run com.discordapp.Discord &
 slack &
-/opt/cisco/secureclient/bin/vpnui
+/opt/cisco/secureclient/bin/vpnui &
 sleep 7
 
 # Start Terminal
