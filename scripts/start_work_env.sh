ec2c331fd846e0dc004f5c4e63f016a294bb396d
Hunter Campbell <hcnureth@gmail.com>
2025-10-15 10:12:09 -0600

    Update work setup

--- scripts/start_work_env.sh
+++ scripts/start_work_env.sh
@@ -3,10 +3,4 @@ flatpak run com.google.Chrome --profile-directory="Profile 2" &
 flatpak run com.discordapp.Discord &
 slack &
 /opt/cisco/secureclient/bin/vpnui &
-sleep 7
-
-# Start Terminal
 kitty &
-sleep 3
-xdotool type "attach vac"
-xdotool key KP_Enter
